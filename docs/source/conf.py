a37737c205598854af03ea15ba3f8c546998b1cb
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-03-27 14:31:57 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,8 @@ import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 os.system("rm -r freenove_Kit")
-os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Ultimate_Starter_Kit_for_Raspberry_Pi freenove_Kit")
-project = 'fnk0020-docs'
+os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Super_Starter_Kit_for_ESP8266 freenove_Kit")
+project = 'fnk0074-docs'
 
 # >>> BEGIN BASE CONFIG (AUTO-GENERATED)
 # !!! DO NOT EDIT THIS SECTION MANUALLY !!!
@@ -22,7 +22,7 @@ copyright = '2016-2025, Freenove'
 author = 'Freenove'
 release = 'v1.0.0'
 version = 'v1.0.0'
-# 测试测试
+# test test
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -133,6 +133,7 @@ def setup(app):
     # app.add_css_file('https://cdn.jsdelivr.net/gh/Freenove/freenove-docs/docs/source/_static/css/custom.css')
 
 # <<< END BASE CONFIG
+
 
 # === BEGIN PROJECT-SPECIFIC CONFIG ===
 # （原来的所有配置内容保持在这里不变）
